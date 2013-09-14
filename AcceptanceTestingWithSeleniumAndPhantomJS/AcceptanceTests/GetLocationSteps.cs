bef8181920bd0a806e4e76a8882afd76e3e65eec
Per Ivar Seterlund <3komma14@gmail.com>
2013-09-14 11:41:38 +0200

    Added specflow scenarios
    And changed specflow to use MsTest as testrunner

---
+++ AcceptanceTestingWithSeleniumAndPhantomJS/AcceptanceTests/GetLocationSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTests
+{
+    [Binding]
+    public class GetLocationSteps
+    {
+        [Given(@"I have opened the webpage")]
+        public void GivenIHaveOpenedTheWebpage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press submit button")]
+        public void WhenIPressSubmitButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"my location should be visible on the screen")]
+        public void ThenMyLocationShouldBeVisibleOnTheScreen()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
